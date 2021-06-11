01791cb247e03a2fd24f2748c95f1ffec96284ed
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-06-11 12:57:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,23 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :tags
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
+
+
+  def tags_attributes=(tags_attributes)
+    
+    tags_attributes.values.each do |tags_attribute|
+      if tags_attribute["name"].present?
+        tag = Tag.find_or_create_by(tags_attribute)
+        self.tags << tags
+    
+      end
+    end
+  end
 end
+
+
+
